cb2f8ca2a832738c6d896ec8302ef52eaf4880ea
Harish Ojha <harish0916ojhacs@gmail.com>
2024-05-16 17:37:50 +0530

    Add Square root

--- index.js
+++ index.js
@@ -10,5 +10,6 @@ console.log(calc.sub(12,10));
 console.log(calc.mul(12,10));
 console.log(calc.div(12,10));
 console.log(calc.mod(12,10));
-console.log(calc.pow(12,10));
+console.log(calc.pow(12,2));
+console.log(calc.sqrt(144,2));
 console.log("thankyou very much, ok bye");
--- util/script.js
+++ util/script.js
@@ -4,3 +4,4 @@ module.exports.mul = (a,b) => a*b;
 module.exports.div = (a,b) => a/b;
 module.exports.mod = (a,b) => a%b;
 module.exports.pow = (a,b) => a**b;
+module.exports.sqrt = (a,b) => a**(1/b);
