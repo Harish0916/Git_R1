c0702973dd0ccfdb3e19f75d20e19d1df40389cd
Harish Ojha <harish0916ojhacs@gmail.com>
2024-05-16 15:21:40 +0530

    calling sum and sub

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const sum = require('./util/script.js');
+const calc = require('./util/script.js');
 
 const name = "Anjali";
 const age = 25;
 const gender = 'Male';
 const job = "Software Engineer"
 
-console.log(sum(12,10));
+console.log(calc.sum(12,10));
+console.log(calc.sub(12,10));
 console.log("thankyou very much, ok bye");
