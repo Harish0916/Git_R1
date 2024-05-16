a8204750d1571ff9a742d0bb2f6b681cbac811dc
Harish Ojha <harish0916ojhacs@gmail.com>
2024-05-16 18:01:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const calc = require('./util/script.js');
 const name = "Anjali Ojha";
 const age = 24;
 const gender = 'Female';
-const job = "Lawyer at Supreme Court"
+const job = "Lawyer at Supreme Court and high court"
 
 console.log(calc.sum(12,10));
 console.log(calc.sub(12,10));
